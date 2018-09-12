73f84df447383ee7e8583a95caa2a46ba4ce0a4c
Aly Sivji <alysivji@gmail.com>
2018-09-12 16:40:04 -0500

    cleaning up

--- app/apispec.py
+++ app/apispec.py
@@ -5,6 +5,7 @@ from .routes import (
     movies_item_resource,
     movies_resource,
     rate_movie_resource,
+    users_exists_resource,
     users_item_resource,
     users_resource,
 )
@@ -35,9 +36,10 @@ spec.definition("UserPath", schema=UserPathSchema)
 
 # parse docstrings for routes
 spec.add_path(resource=login_resource)
-spec.add_path(resource=movies_resource)
-spec.add_path(resource=movies_item_resource)
 spec.add_path(resource=movies_bulk_resource)
+spec.add_path(resource=movies_item_resource)
+spec.add_path(resource=movies_resource)
 spec.add_path(resource=rate_movie_resource)
-spec.add_path(resource=users_resource)
+spec.add_path(resource=users_exists_resource)
 spec.add_path(resource=users_item_resource)
+spec.add_path(resource=users_resource)
